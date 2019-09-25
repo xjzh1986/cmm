456a5e9697c7d94d425498e23fdd581ca76cc09b
xjz <273904882@qq.com>
2019-09-25 10:47:50 +0800

    框架调整

---
+++ cmm-user-entity/src/main/java/com/cn/cmm/CmmUserEntityApplication.java
@@ -0,0 +1,13 @@
+package com.cn.cmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmmUserEntityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmmUserEntityApplication.class, args);
+	}
+
+}
