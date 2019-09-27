b4ef3703b66d4acf082e4a155707db2f62fb21ca
xjz <273904882@qq.com>
2019-09-27 11:27:15 +0800

    配置中心

---
+++ cmm-config-client/src/main/java/com/cn/cmm/CmmConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.cn.cmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class CmmConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CmmConfigClientApplication.class, args);
+	}
+
+}
