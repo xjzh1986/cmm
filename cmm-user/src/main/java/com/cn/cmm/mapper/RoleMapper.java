dc896571ce667c0db0f3cf42d414dac2ccb9ad82
xjz <273904882@qq.com>
2019-09-28 13:56:43 +0800

    添加 role

---
+++ cmm-user/src/main/java/com/cn/cmm/mapper/RoleMapper.java
@@ -0,0 +1,17 @@
+package com.cn.cmm.mapper;
+
+import com.cn.cmm.entity.Role;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    Role selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
