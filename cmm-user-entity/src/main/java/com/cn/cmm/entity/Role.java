9afa4b0f48670e8637c5c6e0666053b605436f53
xjz <273904882@qq.com>
2019-09-28 13:57:09 +0800

    添加 role

---
+++ cmm-user-entity/src/main/java/com/cn/cmm/entity/Role.java
@@ -0,0 +1,23 @@
+package com.cn.cmm.entity;
+
+public class Role {
+    private Long id;
+
+    private String role_name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRole_name() {
+        return role_name;
+    }
+
+    public void setRole_name(String role_name) {
+        this.role_name = role_name == null ? null : role_name.trim();
+    }
+}
